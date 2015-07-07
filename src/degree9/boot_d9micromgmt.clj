c4b63f358d5991ce2733d582a93099c02f340bbd
Matthew Ratzke <matthew.003@me.com>
2015-07-07 15:59:05 -0600

    version 0.2.0

--- src/degree9/boot_d9micromgmt.clj
+++ src/degree9/boot_d9micromgmt.clj
@@ -9,55 +9,20 @@
             [clojure.string     :as string]
             [clojure.java.io    :as io]
             [cheshire.core      :as cheshire]
-            [stencil.core       :as stencil]))
-
-(defn- change-file-ext [path ext]
-  (string/replace path #"\.[^\.]+$" (str "." ext)))
-
-(defn- convert-file-type
-  "Convert files between types."
-  [parser generator ext]
-  (boot/with-pre-wrap fileset
-    (let [tmp      (boot/tmp-dir!)]
-      (doseq [f (->> fileset
-                     boot/input-files)]
-        (let [in-file  (tmpd/file f)
-              in-path  (tmpd/path f)
-              out-path (change-file-ext in-path ext)
-              out-file (io/file tmp out-path)
-              result   (->> (parser (slurp in-file)) generator)]
-          (util/info "Converting files...\n")
-          (doto out-file
-            io/make-parents
-            (spit result))))
-      (-> fileset (boot/add-resource tmp) boot/commit!))))
-
-(defn- yaml-desttype
-  "Convert YAML to another type."
-  [generator
-   ext]
-  (convert-file-type boot/yaml-parse generator ext))
-
-(defn- yaml-json
-  "Convert YAML to JSON."
-  []
-  (yaml-desttype boot/json-generate "json"))
-
-(defn- yaml-psdsc
-  "Convert YAML to PowerShell Desired State Configuration."
-  []
-  (yaml-desttype '(stencil/render-file "psdsc") "ps1"))
+            [stencil.core       :as stencil]
+            [degree9.boot-d9micromgmt.impl :as impl]))
 
 (boot/deftask yaml-to-ARM
   "Convert YAML file to Azure Resource Manager JSON file."
   []
   (let [regex #{ #"(?i)(/.*)*AzureResourceManager/.*\.yaml$" }]
     (comp (tasks/sift :to-source regex :include regex)
-          (yaml-json))))
+          (impl/yaml-json))))
 
 (boot/deftask yaml-to-psdsc
   "Convert YAML file to PowerShell Desired State Configuration."
   []
   (let [regex #{ #"(?i)(/.*)*PSDesiredStateConfiguration/.*\.yaml$" }]
-    (comp (tasks/sift :to-source regex :include regex)
-          (yaml-psdsc))))
+    (comp (impl/import-stencil-templates)
+          (tasks/sift :to-source regex :include regex)
+          (impl/yaml-psdsc))))
---
+++ src/degree9/boot_d9micromgmt/impl.clj
@@ -0,0 +1,64 @@
+(ns degree9.boot-d9micromgmt.impl
+  {:boot/export-tasks true}
+  (:require [boot.core          :as boot]
+            [boot.pod           :as pod]
+            [boot.tmpdir        :as tmpd]
+            [boot.util          :as util]
+            [boot.task.built-in :as tasks]
+            [clj-yaml.core      :as yaml]
+            [clojure.string     :as string]
+            [clojure.java.io    :as io]
+            [cheshire.core      :as cheshire]
+            [stencil.core       :as stencil]
+            [stencil.loader     :as tmplldr]))
+
+(defn- change-file-ext [path ext]
+  (string/replace path #"\.[^\.]+$" (str "." ext)))
+
+(defn- psdsc-generate
+  ""
+  [x]
+  (stencil/render-file "psdsc.mustache" x))
+
+(defmacro srctype-to-desttype
+  ""
+  [parser generator ext]
+  `(boot/with-pre-wrap fileset#
+    (let [tmp# (boot/tmp-dir!)]
+      (doseq [f# (->> fileset#
+                     boot/input-files)]
+        (let [in-file#  (tmpd/file f#)
+              in-path#  (tmpd/path f#)
+              out-path# (change-file-ext in-path# ~ext)
+              out-file# (io/file tmp# out-path#)
+              result#   (-> in-file# slurp (~parser :keywords true) ~generator)]
+          (util/info "Converting files...\n")
+          (doto out-file#
+            io/make-parents
+            (spit result#))))
+      (-> fileset# (boot/add-resource tmp#) boot/commit!))))
+
+(defn yaml-json
+  "Convert YAML to JSON."
+  []
+  (srctype-to-desttype yaml/parse-string boot/json-generate "json"))
+
+(defn yaml-psdsc
+  "Convert YAML to PowerShell Desired State Configuration."
+  []
+  (srctype-to-desttype yaml/parse-string psdsc-generate "ps1"))
+
+(defn import-stencil-templates
+  ""
+  []
+  (boot/with-pre-wrap fileset
+    (let []
+      (doseq [f (->> fileset
+                     boot/input-files
+                     (boot/by-ext [".mustache"]))]
+        (let [in-file (tmpd/file f)
+              in-path (tmpd/path f)]
+          (util/info "Importing stencil templates...\n")
+          (util/info "• %s\n" (.getName in-file))
+          (tmplldr/register-template (.getName in-file) (slurp in-file))))
+      (-> fileset boot/commit!))))
